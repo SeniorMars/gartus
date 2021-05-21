ba68deccb40a723a3e164dcec0611dfaab4f78cf
KarlWithK <jocelinc60@outlook.com>
2021-05-21 13:59:13 -0400

    Rewrote and added vector class

---
+++ src/graphics/vector.rs
@@ -0,0 +1,237 @@
+use std::{
+    fmt::{self, Display},
+    ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Sub, SubAssign},
+};
+
+#[derive(Debug, Clone, Copy)]
+/// A 3D geometric vector
+/// An extra one is added for [Matrix] support
+pub struct Vector {
+    /// Data of the vector
+    pub data: [f64; 4],
+}
+
+#[allow(dead_code)]
+impl Vector {
+    /// Creates a new 3D geometric vector
+    ///
+    /// # Arguments
+    ///
+    /// * `x` - The x corrdinate of the vector
+    /// * `y` - The y corrdinate of the vector
+    /// * `z` - The z corrdinate of the vector
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    /// ```
+    /// use crate::curves_rs::graphics::vector::Vector;
+    /// let vec3 = Vector::new(0.0, 0.0, 0.0);
+    /// ```
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self {
+            data: [x, y, z, 1.0],
+        }
+    }
+
+    /// Produces a new vector based on the cross product of two vectors
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - A vector to be multipled on
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    /// ```
+    /// use crate::curves_rs::graphics::vector::Vector;
+    /// let one = Vector::new(1.0, 1.0, 1.0);
+    /// let two = Vector::new(1.0, 2.0, 3.0);
+    /// let cross = one.cross(two);
+    /// ```
+    pub fn cross(self, other: Vector) -> Self {
+        Self {
+            data: [
+                self[1] * other[2] - self[2] * other[1],
+                self[2] * other[0] - self[0] * other[2],
+                self[0] * other[1] - self[1] * other[0],
+                1.0,
+            ],
+        }
+    }
+
+    /// Does dot multiplation between two vector
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - A vector to be multipled on
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    /// ```
+    /// use crate::curves_rs::graphics::vector::Vector;
+    /// let one = Vector::new(1.0, 1.0, 1.0);
+    /// let two = Vector::new(1.0, 2.0, 3.0);
+    /// let dot = one.dot(two);
+    /// ```
+    pub fn dot(&self, other: Vector) -> f64 {
+        self[0] * other[0] + self[1] * other[1] + self[2] * other[2]
+    }
+
+    /// Returns the mathematical length of a vector
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    /// ```
+    /// use crate::curves_rs::graphics::vector::Vector;
+    /// let one = Vector::new(1.0, 1.0, 1.0);
+    /// let length = one.length();
+    /// ```
+    pub fn length(self) -> f64 {
+        self.dot(self).sqrt()
+    }
+
+    /// Returns a normalized vector
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    /// ```
+    /// use crate::curves_rs::graphics::vector::Vector;
+    /// let one = Vector::new(2.0, 2.0, 2.0);
+    /// let normal = one.normalized();
+    /// ```
+    pub fn normalized(self) -> Self {
+        self / self.length()
+    }
+}
+
+impl Index<usize> for Vector {
+    type Output = f64;
+    fn index(&self, index: usize) -> &f64 {
+        &self.data[index]
+    }
+}
+
+impl IndexMut<usize> for Vector {
+    fn index_mut(&mut self, index: usize) -> &mut f64 {
+        &mut self.data[index]
+    }
+}
+
+impl Add for Vector {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            data: [
+                self[0] + other[0],
+                self[1] + other[1],
+                self[2] + other[2],
+                1.0,
+            ],
+        }
+    }
+}
+
+impl AddAssign for Vector {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            data: [
+                self[0] + other[0],
+                self[1] + other[1],
+                self[2] + other[2],
+                1.0,
+            ],
+        }
+    }
+}
+
+impl Sub for Vector {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            data: [
+                self[0] - other[0],
+                self[1] - other[1],
+                self[2] - other[2],
+                1.0,
+            ],
+        }
+    }
+}
+
+impl SubAssign for Vector {
+    fn sub_assign(&mut self, other: Self) {
+        *self = Self {
+            data: [
+                self[0] - other[0],
+                self[1] - other[1],
+                self[2] - other[2],
+                1.0,
+            ],
+        }
+    }
+}
+
+impl Mul<f64> for Vector {
+    type Output = Vector;
+
+    fn mul(self, other: f64) -> Self::Output {
+        Self {
+            data: [self[0] * other, self[1] * other, self[2] * other, 1.0],
+        }
+    }
+}
+
+impl MulAssign<f64> for Vector {
+    fn mul_assign(&mut self, other: f64) {
+        *self = Self {
+            data: [self[0] * other, self[1] * other, self[2] * other, 1.0],
+        }
+    }
+}
+
+impl Div<f64> for Vector {
+    type Output = Self;
+
+    fn div(self, other: f64) -> Self {
+        Self {
+            data: [self[0] / other, self[1] / other, self[2] / other, 1.0],
+        }
+    }
+}
+
+impl DivAssign<f64> for Vector {
+    fn div_assign(&mut self, other: f64) {
+        *self = Self {
+            data: [self[0] / other, self[1] / other, self[2] / other, 1.0],
+        };
+    }
+}
+
+impl Display for Vector {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "({:.6}, {:.6}, {:.6}, {:.6})",
+            self[0], self[1], self[2], self[3]
+        )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn print_vector() {
+        let one = Vector::new(1.0, 1.0, 1.0);
+        let two = Vector::new(1.0, 2.0, 3.0);
+        let cross = one.cross(two);
+        println!("{}", cross);
+        println!("{:.6}", cross.length())
+    }
+}
