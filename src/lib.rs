29a23c5d6bb7c5f3d28aaab79a0becc6152df1e9
KarlWithK <jocelinc60@outlook.com>
2022-02-16 14:32:22 -0600

    Included a prelude!

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,16 @@
 //! Provides an "art canvas" to work with drawings, a mini matrix library
 //! with several 3D transformations, and a parser to read scripts.
 //! This library is still a work in progress project. Be warn.
+//!
+//! # The prelude
+//!
+//! Importing each trait individually can become a chore, so we provide a
+//! `prelude` module to allow you to import the main traits all at once. For
+//! example:
+//!
+//! ```rust
+//! use curves_rs::prelude::*;
+//! ```
 
 /// A module that includes method to read external PPM and allows them to be used with this system.
 pub mod external;
@@ -15,6 +25,8 @@ pub mod graphics;
 /// This module hosts a [Parser] that allows an image to be created through
 /// a detailed specification. More information found in the module.
 pub mod parser;
+/// prelude with
+pub mod prelude;
 /// This module provides utilities that might be needed to use more
 /// advance features that are not fully integrated into parser
 /// or graphics modules.
---
+++ src/prelude.rs
@@ -0,0 +1,8 @@
+//! Contains common types that can be glob-imported (`*`) for convience
+pub use crate::{
+    gmath::matrix::Matrix,
+    graphics::{
+        colors::{ColorSpace, Rgb},
+        display::Canvas,
+    },
+};
